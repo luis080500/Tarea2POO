169640c5f67c292fee0e96d72ac174f72715a2a6
luis080500 <e1352084949@live.uleam.edu.ec>
2019-11-07 18:58:58 -0500

    Create Pila.cs

---
+++ LuisDelgado3roB/LuisDelgado3roB/TADs/Pila.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuisDelgado3roB.TADs
+{
+    public class Pila
+    {
+
+        private int[] data = new int[5];
+
+        public int[] Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        private int cima = -1;
+
+        public int Cima
+        {
+            get { return cima; }
+            set { cima = value; }
+        }
+        public void EliminarData ()
+        {
+            if (Cima == -1)
+                throw new Exception("No hay datos en la Pila");
+            else
+                Cima--;
+        }
+        public void Anadir(int numero) {
+            if (Cima<(Data.Length-1))
+            {
+                Cima++;
+                Data[Cima] = numero;
+            }
+        
+        
+        
+        }
+
+
+
+    }
+}
