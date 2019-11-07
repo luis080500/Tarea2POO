2ab2307f3b71e6d45a1c982a0e676766dff6d7f5
luis080500 <e1352084949@live.uleam.edu.ec>
2019-11-07 18:58:54 -0500

    Create Cola.cs

---
+++ LuisDelgado3roB/LuisDelgado3roB/TADs/Cola.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuisDelgado3roB.TADs
+{
+    class Cola
+    {
+        private int[] dato = new int[5];
+        public int[] Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        private int ultimo = -1;
+
+        public int Ultimo
+        {
+            get { return ultimo; }
+            set { ultimo = value; }
+        }
+        private int primero = -1;
+
+        public int Primero
+        {
+            get { return primero; }
+            set { primero = value; }
+        }
+
+        public void EliminarDato()
+        {
+            if (Ultimo == -1 && Primero ==-1)
+                throw new Exception("No hay datos en la Pila");
+            else
+            {
+                if (Primero == Ultimo)
+                {
+                    Primero = -1;
+                    Ultimo = -1;
+                }
+                else
+                {
+                    Primero++;
+                }
+            }
+        }
+        public void Anadirdato(int numero)
+        {
+            if (Ultimo < (Dato.Length - 1))
+            {
+                Ultimo++;
+                Dato[Ultimo] = numero;
+            }
+        }
+        
+    }
+}
